88237bda3993ac15de3f28e7481c610d8d4cf66e
Lucas Limeira <lucasalveslm@gmail.com>
2019-06-15 17:39:31 -0300

    added error log

--- y.tab.h
+++ y.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.3.2.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2019 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,55 +31,67 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     INTEGER_LITERAL = 258,
-     FLOAT_LITERAL = 259,
-     BOOLEAN_LITERAL = 260,
-     COMMA = 261,
-     COLON = 262,
-     INT_TYPE = 263,
-     FLOAT_TYPE = 264,
-     BOOLEAN_TYPE = 265,
-     CHAR_TYPE = 266,
-     STRING_TYPE = 267,
-     PLUS = 268,
-     MINUS = 269,
-     TIMES = 270,
-     DIVIDE = 271,
-     UNARY_MINUS = 272,
-     UNARY_PLUS = 273,
-     ID = 274,
-     AND = 275,
-     OR = 276,
-     END = 277,
-     EQUALS = 278,
-     MOD = 279,
-     POW = 280,
-     VARIABLE = 281,
-     BITWISE_AND = 282,
-     BITWISE_OR = 283,
-     LESS_THAN = 284,
-     GREATER_THAN = 285,
-     EQUALS_THAN = 286,
-     LEFT_PARENTHESIS = 287,
-     RIGHT_PARENTHESIS = 288,
-     LEFT_BRACKET = 289,
-     RIGHT_BRACKET = 290,
-     IF = 291,
-     ELSE = 292,
-     FOR = 293,
-     WHILE = 294,
-     RETURN = 295,
-     SWITCH = 296,
-     CASE = 297,
-     DEFAULT = 298,
-     BREAK = 299
-   };
+  enum yytokentype
+  {
+    INTEGER_LITERAL = 258,
+    FLOAT_LITERAL = 259,
+    BOOLEAN_LITERAL = 260,
+    COMMA = 261,
+    COLON = 262,
+    INT_TYPE = 263,
+    FLOAT_TYPE = 264,
+    BOOLEAN_TYPE = 265,
+    CHAR_TYPE = 266,
+    STRING_TYPE = 267,
+    PLUS = 268,
+    MINUS = 269,
+    TIMES = 270,
+    DIVIDE = 271,
+    UNARY_MINUS = 272,
+    UNARY_PLUS = 273,
+    ID = 274,
+    AND = 275,
+    OR = 276,
+    END = 277,
+    EQUALS = 278,
+    MOD = 279,
+    POW = 280,
+    VARIABLE = 281,
+    BITWISE_AND = 282,
+    BITWISE_OR = 283,
+    LESS_THAN = 284,
+    GREATER_THAN = 285,
+    EQUALS_THAN = 286,
+    LEFT_PARENTHESIS = 287,
+    RIGHT_PARENTHESIS = 288,
+    LEFT_BRACKET = 289,
+    RIGHT_BRACKET = 290,
+    IF = 291,
+    ELSE = 292,
+    FOR = 293,
+    WHILE = 294,
+    RETURN = 295,
+    SWITCH = 296,
+    CASE = 297,
+    DEFAULT = 298,
+    BREAK = 299
+  };
 #endif
 /* Tokens.  */
 #define INTEGER_LITERAL 258
@@ -127,23 +137,27 @@
 #define DEFAULT 298
 #define BREAK 299
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 10 "parser.y"
+
+union YYSTYPE
 {
+#line 10 "parser.y" /* yacc.c:1921  */
+
     int iValue;
     struct node * npValue;
-}
-/* Line 1529 of yacc.c.  */
-#line 142 "y.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 151 "y.tab.h" /* yacc.c:1921  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
